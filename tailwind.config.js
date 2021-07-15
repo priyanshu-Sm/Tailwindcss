6894d36af34375ad609be2c2aa00f276da58d536
Priyanshu Tiwari <74772584+priyanshu-Sm@users.noreply.github.com>
2021-07-15 12:23:23 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      fontSize:{
+        '8xl': '6rem'
+      },
+      spacing:{
+        '144':'36rem'      }
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
